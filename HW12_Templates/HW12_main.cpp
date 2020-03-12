3bcb2280b408834eae4f4e4e828006e3a0bafab1
Kapurin_V <kapurin@mail.ru>
2020-03-12 11:21:18 +0200

    [HW12 - Correct template function for char data type. Date:20200312]

--- HW12_Templates/HW12_main.cpp
+++ HW12_Templates/HW12_main.cpp
@@ -5,43 +5,36 @@
 #include <iostream>
 #include <conio.h>
 #include <vector>
+#include <sstream> // для stringstream
 
 using namespace std;
 
 
 template <typename T>
-T sum_all(T Name)
+void sum_all(T Name)
 {
-	T RetN = Name;
-
-	return RetN;
+	cout << Name << endl;
 }
 
 
 template <typename T>
-T sum_all(vector<T> VName)
+void sum_all(vector<T> VName)
 {
+	T Sum = VName[0];
+	for (size_t i = 1; i < VName.size(); i++)
+	{
+		Sum = Sum + VName[i];
+	}
+	cout << Sum << endl;
+}
 
-	auto Sum = VName[0];
-	//if (sizeof(char) != sizeof(T))
-	//{
-		for (size_t i = 1; i < VName.size(); i++)
-		{
-			Sum = Sum + VName[i];
-		}
-		return Sum;
-	//}
-	//else if (sizeof(char) == sizeof(T))
-	//{
-	//	//int N = VName.size();
-	//	char Arr1[];
-	//	for (size_t i = 0; i < VName.size(); i++)
-	//	{
-	//		Arr1[i] = VName[i];
-	//	}
 
-	//	return Arr1[VName.size()];
-	//}
+void sum_all(vector<char> VName)
+{
+	std::stringstream res;
+	for (auto i : VName)
+		res << i;
+	cout << res.str() << endl;
 }
 
 
@@ -54,13 +47,12 @@ int main()
 	vector<char> v4 = { 'a', 'b', 'c' };
 
 
-	cout << sum_all(5) << endl;
-	cout << sum_all(3.0) << endl;
-	cout << sum_all(v1) << endl;
-	cout << sum_all(v2) << endl;
-	cout << sum_all(v3) << endl;
-//	cout << static_cast <string>(sum_all(v4)) << endl;
-	cout << sum_all(v4) << endl;
+	sum_all(5);
+	sum_all(3.0);
+	sum_all(v1);
+	sum_all(v2);
+	sum_all(v3);
+	sum_all(v4);
 
 	_getch();
 	return 0;
